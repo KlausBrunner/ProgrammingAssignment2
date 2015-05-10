c95a492c5099a2ae2e806e67ec4aa13a5d3999b6
Klaus Brunner <klaus.brunner@gmail.com>
2015-05-10 14:12:53 +0200

    first (superficially tested) working version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using R's scoping rules and first-class functions to cache values.
+## Coursera R Programming Course (Roger Peng), Programming Assignment 2
 
-## Write a short comment describing this function
-
+## Return a vector of functions that store a matrix and its associated inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    
+    setinv <- function(i) inv <<- i
+    getinv <- function() inv
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of the matrix x as defined by cacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    matrix <- x$get()
+    inv <- solve(matrix, ...)
+    x$setinv(inv)
+    inv
 }
